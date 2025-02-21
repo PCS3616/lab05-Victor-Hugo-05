9b9ae5cd9f59de8398dfdb0e2ee2cea2be297342
Victor-Hugo-05 <150465263+Victor-Hugo-05@users.noreply.github.com>
2025-02-21 17:27:12 +0000

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -5,18 +5,19 @@ START   LD N
         HM =0             
 
 @ /100 
-N      K /0001
+N      K /0004
 RES    K /0001
 VAL    K =1
 
 @ /200
        K =0
-       JZ /214
+       JZ /216
        ML RES
        MM RES
        LD N
        SB VAL
        MM N
+       JP /202
        RS /200
 
 @ /400
