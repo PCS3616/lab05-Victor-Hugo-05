5575f27d93859f8c5ff9f3dc46d42187c3c6996b
Victor-Hugo-05 <150465263+Victor-Hugo-05@users.noreply.github.com>
2025-02-21 17:42:26 -0300

    Add files via upload

---
+++ submission/quadrados-perfeitos.asm
@@ -0,0 +1,31 @@
+@ /000              
+START   LD SOMA
+	MM /102
+	LD POS
+	AD DOIS
+	MM POS
+	LD SOMA
+	AD NUM
+	AD NUM
+	AD UM
+	MM SOMA
+	LD NUM 
+	AD UM
+	MM NUM
+	LD POS
+	AD /9102
+	MM /002
+	LD POS
+	SB LIM
+	JZ FIM
+	JP START
+FIM     HM =0             
+
+
+@ /400
+UM     K =1
+DOIS   K =2
+SOMA   K =1
+NUM    K =1
+LIM    K =124
+POS    K =0
